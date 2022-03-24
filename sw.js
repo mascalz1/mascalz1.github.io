8e891d8a5827c63a9ff425fd136d8fa5a68953c3
mascalz1 <am@amerola.info>
2022-03-24 21:43:12 +0100

    from code

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220321.1158';
+const cacheName = 'chirpy-20220324.2141';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
